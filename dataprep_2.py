0e67a22d4365c417c2b494de91c906efa219161a
amountainclimber004 <dcs694@mocs.utc.edu>
2025-02-26 21:00:59 -0500

    Delete dataprep_2.py

--- dataprep_2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load data
-try:
-    df = pd.read_excel('Ames_Housing.xlsx')
-    print("File loaded successfully!")
-except Exception as e:
-    print(f"Error loading file: {e}")
-    exit()
