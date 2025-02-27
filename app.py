10389b88067eca6143b75689a66b1468cbdd269d
amountainclimber004 <dcs694@mocs.utc.edu>
2025-02-26 23:11:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ model = joblib.load("house_price_model.pkl")
 st.title("Ames Housing Price Prediction App")
 
 st.sidebar.header("Enter House Features:")
-overall_qual = st.sidebar.slider("Overall Quality (1-10)", 1, 10, 5)
+overall_qual = st.sidebar.slider("Overall Quality (Poor-High)", 1, 10, 5)
 gr_liv_area = st.sidebar.number_input("Above Ground Living Area (sq ft)", min_value=500, max_value=5000, value=1500)
-garage_cars = st.sidebar.slider("Number of Garage Cars", 0, 5, 2)
+garage_cars = st.sidebar.slider("Number of Cars in the Garage", 0, 5, 2)
 total_bsmt_sf = st.sidebar.number_input("Total Basement Area (sq ft)", min_value=0, max_value=3000, value=800)
 year_built = st.sidebar.number_input("Year Built", min_value=1800, max_value=2024, value=2000)
 
@@ -25,5 +25,5 @@ if st.sidebar.button("Predict Price"):
     st.write(f"💰 ${predicted_price:,.2f}")
 
 st.sidebar.markdown("---")
-st.sidebar.text("Built with ❤️ using Streamlit")
+st.sidebar.text("Built for Streamlit")
 
